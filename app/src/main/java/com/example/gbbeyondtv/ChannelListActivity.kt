8c2fe2e3f418b3fa0c07735ffae67ae2d89e9791
HawaiinYeti <hawaiinyeti@gmail.com>
2025-01-11 14:47:35 -0800

    Cleanup

--- app/src/main/java/com/example/gbbeyondtv/ChannelListActivity.kt
+++ app/src/main/java/com/example/gbbeyondtv/ChannelListActivity.kt
@@ -41,8 +41,8 @@ class ChannelListActivity : GBBActivity() {
         apiService = RetrofitClient.getClient(baseUrl).create(ApiService::class.java)
         channelService = ChannelService(this, apiService, sharedPreferences)
 
-//        fetchChannels()
         progressBar = findViewById(R.id.progress_bar)
+
         newFetchChannels()
 
         val settingsButton: Button = findViewById(R.id.settings_button)
@@ -52,7 +52,7 @@ class ChannelListActivity : GBBActivity() {
         }
     }
 
-    fun newFetchChannels() {
+    private fun newFetchChannels() {
         progressBar.visibility = ProgressBar.VISIBLE
 
         channelService.fetchChannels(
@@ -66,68 +66,7 @@ class ChannelListActivity : GBBActivity() {
         )
     }
 
-//    fun fetchChannels() {
-//        apiService.getChannels().enqueue(object : Callback<JsonElement> {
-//            override fun onResponse(call: Call<JsonElement>, response: Response<JsonElement>) {
-//                if (response.isSuccessful) {
-//                    val jsonElement = response.body()
-//                    if (jsonElement != null) {
-//                        parseJson(jsonElement)
-//                    } else {
-//                        Toast.makeText(this@ChannelListActivity, "No channels available", Toast.LENGTH_SHORT).show()
-//                    }
-//                } else {
-//                    Toast.makeText(this@ChannelListActivity, "Failed to load channels", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<JsonElement>, t: Throwable) {
-//                Toast.makeText(this@ChannelListActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
-//            }
-//        })
-//    }
-//
-//    private fun parseJson(jsonElement: JsonElement) {
-//        val jsonObject = jsonElement.asJsonObject
-//        val channels = mutableListOf<Channel>()
-//
-//        for (entry in jsonObject.entrySet().sortedBy { it.key }) {
-//            val channelData = entry.value.asJsonObject
-//            val channelJson = channelData.getAsJsonObject("channel")
-//            val queueJson = channelData.getAsJsonArray("queue")
-//
-//            val queue = mutableListOf<QueueItem>()
-//            val host = sharedPreferences.getString("HOST", "")
-//            val port = sharedPreferences.getString("PORT", "")
-//
-//            for (queueItemJson in queueJson) {
-//                val queueData = queueItemJson.asJsonObject.getAsJsonObject("queue_item")
-//                val videoData = queueItemJson.asJsonObject.getAsJsonObject("video")
-//                val url = if (queueItemJson.asJsonObject.get("url").asString.startsWith("http")) {
-//                    queueItemJson.asJsonObject.get("url").asString
-//                } else {
-//                    "http://$host:$port/${queueItemJson.asJsonObject.get("url").asString}"
-//                }
-//
-//                queue.add(QueueItem(
-//                    videoData.asJsonObject.get("name").asString,
-//                    videoData.asJsonObject.get("deck").asString,
-//                    url,
-//                    ZonedDateTime.parse(queueData.asJsonObject.get("start_time").asString),
-//                    ZonedDateTime.parse(queueData.asJsonObject.get("finish_time").asString)
-//                ))
-//            }
-//            val channel = Channel(
-//                channelJson.get("name").asString,
-//                queue
-//            )
-//            channels.add(channel)
-//        }
-//
-//        setupRecyclerView(channels)
-//    }
-
-     fun setupRecyclerView(channels: List<Channel>) {
+     private fun setupRecyclerView(channels: List<Channel>) {
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = ChannelAdapter(channels) { channel ->
